c206435b64652fec41296093f92e5ecbf01abac3
Khant Maung <maungkhantdelica@gmail.com>
2024-07-05 18:07:45 +0200

    pepsi landing page

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgSwitcher(img) {
+  document.querySelector(".pepsi").src = img;
+}
+
+function changeBackgroundColor(color) {
+  const backgroundColor = document.querySelector(".sec");
+  backgroundColor.style.background = color;
+}
